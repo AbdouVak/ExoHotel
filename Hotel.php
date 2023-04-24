e9faf05972a6550b12a63613ef8123f11c893b4c
AbdouVak <abdou.vakouiev@gmail.com>
2023-04-24 17:00:38 +0200

    ajout de la method afficherReservation et afficherInfo

--- Hotel.php
+++ Hotel.php
@@ -4,21 +4,18 @@ class Hotel{
     private string $_nom;
     private array $_address;
     private int $_nbEtoile;
-    private int $_nbChambre;    
+    private int $_nbChambre;
     private int $_nbChambreReserver;
     private int $_nbChambreDispo;
+    private array $_chambres;
 
 
-    public function __construct(string $nom,array $address,int $nbEtoile,int $nbChambre,int $nbChambreReserver, int $nbChambreDispo){
+    public function __construct(string $nom,array $address,int $nbEtoile,int $nbChambre){
         $this->_nom = $nom;
         $this->_address = $address;
         $this->_nbEtoile= $nbEtoile;
-        $this->_nbChambre = $nbChambre;        
-        $this->_nbChambreReserver = $nbChambreReserver;
-        $this->_nbChambreDispo = $nbChambreDispo;
-
-        $this->_realisateur->setFilm($this);
-        $this->_genre->addFilms($this);
+        $this->_nbChambre = $nbChambre;
+        $this->_chambres = [];
     }
 
     public function getNom() :string{
@@ -45,24 +42,36 @@ class Hotel{
     public function getNbChambre():int{
         return $this->_nbChambre;
     }    
-    public function SetNbChambre($nbChambre) {
-        $this->_nbChambre = $nbChambre;
-    }
 
-    public function getNbChambreReserver() :int{
-        return $this->_nbChambreReserver;
-    }    
-    public function setNbChambreReserver($nbChambreReserver){
-        $this->_nbChambreReserver = $nbChambreReserver;
+    public function addChambres(Chambre $chambre){
+        $this->_chambres[] = $chambre;
     }
-
-    public function getNbChambreDispo() :int{
-        return $this->_nbChambreDispo;
-    }    
-    public function setNbChambreDispo($nbChambreDispo){
-        $this->_nbChambreDispo = $nbChambreDispo;
-    }
-
     
+    public function afficherReservation(){
+        $result ="";
+        echo "<h2>Réservation de l'hôtel $this->_nom:</h2><br>";
+        foreach($this->_chambres as $chambre){
+            $result .= "<br>";
+        }
+        return $result."<br>";
+    }
+
+    public function afficherInfo(){
+        $afficherEtoile ="";
+        for($i=0;$i<$this->_nbEtoile;$i++){
+            $afficherEtoile .= "*";
+        }
+        return "<h2>".$this->_nom." ".$afficherEtoile." ".$this->_address[3]."</h2>".
+                $this->_address[0]." ".$this->_address[1]." ".$this->_address[2]." ".$this->_address[3].
+                "<br>Nombre de chambres : $this->_nbChambre <br>";
+    }
+
+    public function __toString(){
+        $afficherEtoile ="";
+        for($i=0;$i<$this->_nbEtoile;$i++){
+            $afficherEtoile .= "*";
+        }
+        return $this->_nom." ".$afficherEtoile." ".$this->_address[3];
+    }
 }
 ?>
