66e5e7009cc54c8460a3a9e4f55f7eb6f8f92e58
AbdouVak <abdou.vakouiev@gmail.com>
2023-04-25 10:08:40 +0200

    Ajout de fonction pour obternir les reservation et les chambre

--- Chambre.php
+++ Chambre.php
@@ -4,20 +4,21 @@ class Chambre{
     private Hotel $_hotel;
     private int $_lits;
     private int $_prix;
-    private bool $_wifi;
+    private bool $_is_wifi;
     private bool $_reserver = false;
     private array $_reservation;
+    public static $counter = 0;
 
     public function __construct(Hotel $hotel,int $nChambre,int $lits, int $prix,bool $wifi){
         $this->_hotel = $hotel;
         $this->_nChambre = $nChambre;
         $this->_lits = $lits;
         $this->_prix = $prix;
-        $this->_wifi = $wifi;
-        $this->_reserver = true;
+        $this->_is_wifi = $wifi;
         $this->_reservation =[];
 
         $this->_hotel->addChambres($this);
+        self::$counter++;
     }
 
     public function getNChambre(){
@@ -33,20 +34,32 @@ class Chambre{
     public function setHotel(Hotel $hotel){
         $this->_hotel = $hotel;
     }
+
+    public function getReservation(){
+        return $this->_reservation;
+    }
     public function addReservation(Reservation $reservation){
         $this->_reservation[] = $reservation;
     }
 
+    public function getNbrChambre(){
+        return self::$counter;
+    }
+
+    public function setReserver($true){
+        $this->_reserver = $true;
+    }
+
     public function __toString(){
         $wifi="";
-        if($this->_wifi){
+        if($this->_is_wifi){
             $wifi="oui";
         }else{
             $wifi="non";
         }
-        return "Chambre:".$this->_nChambre .
-                "(".$this->_lits."lits -".
-                $this->_prix." € - Wifi ". 
+        return " Chambre: ".$this->_nChambre .
+                " (".$this->_lits." lits - ".
+                $this->_prix." € - Wifi : ". 
                 $wifi." )";
     }
 
