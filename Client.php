3e9c5da853b4c457a10ee5cd9dc4a6dae176100b
AbdouVak <abdou.vakouiev@gmail.com>
2023-04-25 12:03:08 +0200

    ajout du trie des reservation

--- Client.php
+++ Client.php
@@ -1,11 +1,11 @@
 <?PHP
 class Client{
     private string $_nom;
-    private array $_reservation;
+    private array $_reservations;
 
     public function __construct(string $nom){
         $this->_nom = $nom;
-        $this->_reservation = [];
+        $this->_reservations = [];
 
     }
 
@@ -17,25 +17,34 @@ class Client{
     }
 
     public function addReservation(Reservation $reservation){
-        $this->_reservation[] = $reservation;
+        $this->_reservations[] = $reservation;
     }
 
     /*-----------------------------------Affiche le nombre les chambres réserver-----------------------------------*/
     public function afficherReservation(){
         $NbrChambre ="";
         $prixTotal =0;
-        $result ="";
+        $result ="";   
+
+      
+
         echo "<h2>Réservation de $this->_nom:</h2>";
 
-        foreach($this->_reservation as $reservation){
+        usort($this->_reservations, function ($a, $b)
+            {
+                return (int) ($a->getDateDebut() > $b->getDateDebut());
+            } );
+
+        foreach($this->_reservations as $reservation){
             $result .= "Hotel: ".$reservation->getNChambre()->getHotel()."/ ".$reservation->getNChambre()." du ".$reservation->getDateDebut()->format("d-m-Y")." au ".$reservation->getDateFin()->format("d-m-Y")."<br>";
+            
             $prixTotal = $prixTotal + $reservation->getNChambre()->getPrix()*intval($reservation->sejour());
             $NbrChambre++;
         }
         return $result = $NbrChambre." RÉSERVATION<br>".$result." Total: ".$prixTotal." €";
     }
 
-    
+
     public function __toString(){
         return $this->_nom;
     }
