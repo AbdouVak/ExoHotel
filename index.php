4ff28998a88553b7bd63aad0a1d02f590086334a
AbdouVak <abdou.vakouiev@gmail.com>
2023-04-25 12:03:18 +0200

    teste du code

--- index.php
+++ index.php
@@ -3,7 +3,22 @@
 <p>A partir de ces captures d’écran, réaliser l’application en POO permettant la gestion de réservations de chambres par des clients dans différents hôtels :</p>
 
 <h2>Résultat</h2>
+<style>
 
+td,
+th {
+
+    padding-left: 10px;
+    padding-right: 10px;
+
+}
+
+table {
+    border-collapse: collapse;
+    border: 1px solid black;
+}
+
+</style>
 <?PHP
 include('Hotel.php');
 include('Client.php');
@@ -24,7 +39,7 @@ $GeorgeL = new Client("George LUCAS");
 $LukeS = new Client("Luke SKYWALKER");
 
 $GL_reserv_C_2 = new Reservation($GeorgeL,$chambre_2,"2020-01-18","2020-01-25");
-$LS_reserv_C_3 = new Reservation($LukeS,$chambre_3,"2020-03-12","2020-12-15");
+$LS_reserv_C_3 = new Reservation($LukeS,$chambre_3,"2020-03-12","2020-03-15");
 $LS_reserv_C_5 = new Reservation($LukeS,$chambre_5,"2020-02-3","2020-02-08");
 
 
@@ -32,4 +47,6 @@ echo $Hilton->afficherInfos();
 echo $Hilton->afficherReservations();
 echo $GeorgeL->afficherReservation();
 echo $LukeS->afficherReservation();
+echo $Hilton->displayStatusChambre();
+
 ?>
