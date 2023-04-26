ac24e44d3572211d30bcaa720e54a3170e5d308d
AbdouVak <abdou.vakouiev@gmail.com>
2023-04-26 08:37:32 +0200

    teste code

--- index.php
+++ index.php
@@ -5,12 +5,9 @@
 <h2>Résultat</h2>
 <style>
 
-td,
-th {
-
+td,th {
     padding-left: 10px;
     padding-right: 10px;
-
 }
 
 table {
@@ -26,7 +23,7 @@ include('Chambre.php');
 include('Reservation.php');
 
 $Hilton = new Hotel("Hilton","10 route de la Gare 67000 Strasbourg",4);
-$Regent = new Hotel("Regent","7 rue de Paris 93000 Pais",3);
+$Regent = new Hotel("Regent","7 rue de Paris 93000 Paris",3);
 
 $chambre_1 = new Chambre($Hilton,1,2,120,false);
 $chambre_2 = new Chambre($Hilton,2,1,120,false);
@@ -38,12 +35,11 @@ $chambre_6 = new Chambre($Hilton,6,1,300,true);
 $GeorgeL = new Client("George LUCAS");
 $LukeS = new Client("Luke SKYWALKER");
 
-$GL_reserv_C_2 = new Reservation($GeorgeL,$chambre_1,"2020-01-18","2020-01-25");
-$LS_reserv_C_6 = new Reservation($LukeS,$chambre_6,"2020-03-12","2020-03-15");
-$LS_reserv_C_5 = new Reservation($LukeS,$chambre_5,"2020-02-03","2020-02-08");
+$GL_reserv_C_1 = new Reservation($GeorgeL,$chambre_1,"2020-01-18","2020-01-25");
+$LS_reserv_C_3 = new Reservation($LukeS,$chambre_3,"2020-03-12","2020-03-15");
+$LS_reserv_C_6 = new Reservation($LukeS,$chambre_6,"2020-02-03","2020-02-08");
 
 
-// var_dump($chambre_2->getReservation()[0]);die;
 echo $Hilton->afficherInfos();
 echo $Regent->afficherReservations();
 echo $Hilton->afficherReservations();
